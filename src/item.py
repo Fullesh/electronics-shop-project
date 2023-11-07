79d9f1200bfc3c938bb891bbf35261a59c263487
Danila Belov <danilabelov.adm@yandex.ru>
2023-11-07 20:16:58 +0300

    Modules of the Items class have been added

--- src/item.py
+++ src/item.py
@@ -5,6 +5,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
+
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
@@ -13,7 +14,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
+
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +26,13 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        total_price = self.price * self.quantity
+        return total_price
 
-    def apply_discount(self) -> None:
+
+    def apply_discount(self) -> float:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+        return self.price
