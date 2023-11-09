4f68ca8da3a94e04bf627b45825c2fb1a6c2995d
Danila Belov <danilabelov.adm@yandex.ru>
2023-11-09 14:06:42 +0300

    As part of the second homework, modules were added for reading from csv files and converting strings to integers using access modes and @property decorators

--- src/item.py
+++ src/item.py
@@ -1,10 +1,12 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
-
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -14,11 +16,9 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all.append(self)
-
 
     def calculate_total_price(self) -> float:
         """
@@ -29,10 +29,41 @@ class Item:
         total_price = self.price * self.quantity
         return total_price
 
-
     def apply_discount(self) -> float:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
         return self.price
+
+    @property
+    def get_name(self):
+        return self.__name
+
+    @get_name.setter
+    def set_name(self, product_name_str):
+        if len(product_name_str) > 10:
+            print('Длина наименования товара превышает 10 символов')
+            self.__name = product_name_str[:10]
+        else:
+            self.__name = product_name_str
+
+    @staticmethod
+    def string_to_number(number_string):
+        return int(float(number_string))
+
+    @classmethod
+    def instantiate_from_csv(cls, directory):
+        cls.all.clear()
+        with open(directory, 'r') as csv_file:
+            file = csv.DictReader(csv_file)
+            for i in file:
+                name = i['name']
+                price = float(i['price'])
+                quantity = int(i['quantity'])
+                item = cls(str, float, int)
+                item.name = name
+                item.price = price
+                item.quantity = quantity
+                cls.all.append(item)
+
