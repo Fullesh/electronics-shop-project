b07479021afbd7a0dcf66aaa4bfce67b3931a0a3
Danila Belov <danilabelov.adm@yandex.ru>
2023-11-20 21:30:42 +0300

    Added a test for the magic method __add__. Total coverage 90%

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -11,6 +11,9 @@ def test_str():
     assert str(phone1) == 'iPhone 14'
 
 
+def test_add():
+    assert phone1 + phone1 == 10
+    assert phone1 + 2121 == 'Невозможно сложить с экземплярами не Phone или Item классов'
 
 
 
