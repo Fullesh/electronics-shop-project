3bae7ee1acade7c1d0138eaa5d3f747d2c739c6c
Danila Belov <danilabelov.adm@yandex.ru>
2023-11-27 13:22:57 +0300

    added tests for getter and setter for class Phone. Coverage = 92%

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.phone import Phone
 
 phone1 = Phone('iPhone 14', 120000, 5, 2)
@@ -11,9 +13,13 @@ def test_str():
     assert str(phone1) == 'iPhone 14'
 
 
-def test_add():
-    assert phone1 + phone1 == 10
-    assert phone1 + 2121 == 'Невозможно сложить с экземплярами не Phone или Item классов'
-
+def test_number_of_sim():
+    phone1 = Phone('iPhone 14', 120000, 5, 2)
+    assert phone1.number_of_sim == 2
+    phone1.number_of_sim = 3
+    assert phone1.number_of_sim == 3
+    with pytest.raises(ValueError, match="Количество физических SIM-карт должно быть целым числом больше нуля."):
+        phone1.number_of_sim = -1
+        phone1.number_of_sim = 0
 
 
