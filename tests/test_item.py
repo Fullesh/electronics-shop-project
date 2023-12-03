bfa7b49afb05a8e000cc208f6a8b5482a706030b
Danila Belov <danilabelov.adm@yandex.ru>
2023-12-03 17:57:39 +0300

    Added tests for Exceptions. Coverage 89%

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,7 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-from src.item import Item
+import pytest
+
+from src.item import Item, InstantiateCSVError
 from src.phone import Phone
 
 item1 = Item("Телефон", 60000, 20)
@@ -28,6 +30,7 @@ def test_instantiate_from_csv():
     item2 = Item.all[0]
     assert item2.name == 'Смартфон'
     assert len(item2.all) == 5
+    assert Item.instantiate_from_csv() is None
 
 
 def test_set_name():
@@ -46,3 +49,16 @@ def test_str():
 def test_add():
     assert item1 + phone1 == 25
     assert item1 + 2131 == 'Невозможно сложить с экземплярами не Phone или Item классов'
+
+
+def test_error_instantiate_from_csv():
+    with pytest.raises(FileNotFoundError):
+        Item.instantiate_from_csv()
+
+
+def test_error2_instantiate_from_csv(tmpdir):
+    bad_data_file = tmpdir.join("items.csv")
+    bad_data_file.write('name,price\nitem1,200\n')
+
+    with pytest.raises(InstantiateCSVError):
+        Item.instantiate_from_csv(str(bad_data_file))
